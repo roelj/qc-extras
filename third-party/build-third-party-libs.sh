79a2b7fe1c6fd2952b10053a4b1f6106963014f1
Roel Janssen <roel@gnu.org>
2023-01-29 21:16:07 +0100

    third-party: Add build procedure for 'gmp'.
    * third-party/build-third-party-libs.sh: Add build procedure for 'gmp'.

--- third-party/build-third-party-libs.sh
+++ third-party/build-third-party-libs.sh
@@ -31,6 +31,16 @@ make
 make install
 cd ..
 
+## GMP
+## ----------------------------------------------------------------------------
+curl -LO https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
+tar axvf gmp-6.2.1.tar.xz
+cd gmp-6.2.1
+./configure CC=${CROSS_GCC} --host=arm-linux-gnueabi --prefix="${BUILD_OUTPUT_DIR}"
+make
+make install
+cd ..
+
 ## LIBCURL
 # -----------------------------------------------------------------------------
 # Build without SSL, we will use the library installed on the Quad Cortex
